7f67e263250886c3282b87721e78b4bd16f4ab3c
temptationkentos57 <kreetlannoo@hotmail.com>
2025-09-05 20:24:17 +0200

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./src/config/db');
+const userRoutes = require('./src/routes/userRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Kết nối đến cơ sở dữ liệu
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/api/users', userRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server đang chạy trên cổng ${PORT}`);
+});
